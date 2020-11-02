7739150b45d89d243f441d311af4b0b0fedd57bc
Hadi El Ali <Hadi.el.ali@fau.de>
2020-11-02 19:32:55 +0100

    Importing Project

---
+++ Essens_Anwendung/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Essens_Anwendung.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList_Name" runat="server">
+                <asp:ListItem>R</asp:ListItem>
+                <asp:ListItem>S</asp:ListItem>
+                <asp:ListItem>H</asp:ListItem>
+                <asp:ListItem>N</asp:ListItem>
+                <asp:ListItem>M</asp:ListItem>
+                <asp:ListItem>N</asp:ListItem>
+            </asp:DropDownList>
+            <asp:DropDownList ID="DropDownList_Essen" runat="server">
+                <asp:ListItem>Apfel</asp:ListItem>
+                <asp:ListItem>Birne</asp:ListItem>
+                <asp:ListItem>Orange</asp:ListItem>
+            </asp:DropDownList>
+            <asp:DropDownList ID="DropDownList_Zusatz" runat="server">
+                <asp:ListItem>Gut</asp:ListItem>
+                <asp:ListItem>Schlecht</asp:ListItem>
+                <asp:ListItem>Super</asp:ListItem>
+            </asp:DropDownList>
+            <asp:Button ID="Button_Auswerten" runat="server" OnClick="Button_Auswerten_Click" Text="Auswerten" />
+            <asp:Button ID="Button_Löschen" runat="server" OnClick="Button_Löschen_Click" Text="Löschen" Width="76px" />
+            <br />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Essens_Anwendung/WebForm1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Essens_Anwendung
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Source\Repos\Essens_Anwendung\Essens_Anwendung\App_Data\Database1.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State==ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            disp_data();
+        }
+
+        protected void Button_Auswerten_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+
+            string Essen = DropDownList_Essen.SelectedItem.Text;
+            String Name = DropDownList_Name.SelectedItem.Text;
+            String Zusatz = DropDownList_Zusatz.SelectedItem.Text;
+
+            cmd.CommandText =   "insert into table1 values('" + Name + "','" + Essen + "','" + Zusatz + "')";
+            cmd.ExecuteNonQuery();
+            disp_data();
+
+           
+        }
+        public void disp_data()
+        {
+            SqlCommand cmd = new SqlCommand(/*@"INSERT INTO [Table](Name,Essen,Zusatz) VALUES (@Name,@Essen,@Zusatz)"+*/);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select * from table1";
+            cmd.ExecuteNonQuery();
+            
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button_Löschen_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+
+            string Essen = DropDownList_Essen.SelectedItem.Text;
+            String Name = DropDownList_Name.SelectedItem.Text;
+            String Zusatz = DropDownList_Zusatz.SelectedItem.Text;
+
+            cmd.CommandText = "delete from table1 where Name='" + Name + "'";
+            cmd.ExecuteNonQuery();
+
+            disp_data();
+        }
+    }
+}
